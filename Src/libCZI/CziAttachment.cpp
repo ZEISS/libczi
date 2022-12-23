e0b0b3682416cb852f335a1b58dd250a71f31aee
ptahmose <jbohl@h-quer.de>
2022-12-23 23:24:14 +0100

    cosmetic

--- Src/libCZI/CziAttachment.cpp
+++ Src/libCZI/CziAttachment.cpp
@@ -3,9 +3,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
 #include "stdafx.h"
-#include "CziAttachment.h"
-
 #include <utility>
+#include "CziAttachment.h"
 #include "CziUtils.h"
 
 using namespace libCZI;
