2b1444af2240ac1c935a4a3b6e049eaa98d45ae7
J. Bohl <juergen.bohl@zeiss.com>
2023-03-23 09:57:56 +0100

    editorconfig-checker...

--- Src/libCZI/pugiconfig.hpp
+++ Src/libCZI/pugiconfig.hpp
@@ -17,7 +17,7 @@
 #ifndef HEADER_PUGICONFIG_HPP
 #define HEADER_PUGICONFIG_HPP
 
- // Uncomment this to enable wchar_t mode
+// Uncomment this to enable wchar_t mode
 #define PUGIXML_WCHAR_MODE
 
 // Uncomment this to enable compact mode
