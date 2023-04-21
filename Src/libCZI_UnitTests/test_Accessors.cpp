c6b2969cb569ccaa6d58dffbbca25c0cfb18aa91
ptahmose <jbohl@h-quer.de>
2023-04-21 22:26:48 +0200

    add tests with "sortByM=false"

--- Src/libCZI_UnitTests/test_Accessors.cpp
+++ Src/libCZI_UnitTests/test_Accessors.cpp
@@ -108,7 +108,7 @@ static tuple<shared_ptr<void>, size_t> CreateTestCziDocumentAndGetAsBlob(const a
 
 struct ZOrderAndResultGray8Fixture : public testing::TestWithParam<tuple<int, int, int, array<uint8_t, 4>>> { };
 
-TEST_P(ZOrderAndResultGray8Fixture, CreateDocumentAndUseSingleChannelScalingTileAccessorAndCheckResult)
+TEST_P(ZOrderAndResultGray8Fixture, CreateDocumentAndUseSingleChannelScalingTileAccessorWithSortByMAndCheckResult)
 {
     // We create a document with 3 subblocks, where the M-index (of each subblock) is given by the test parameters.
     // The subblocks are 2x1 pixels, and the pixel values are 42 for the 1st, 45 for the second, and 47 for the third
@@ -142,7 +142,7 @@ TEST_P(ZOrderAndResultGray8Fixture, CreateDocumentAndUseSingleChannelScalingTile
     EXPECT_EQ(p[3], expected[3]);
 }
 
-TEST_P(ZOrderAndResultGray8Fixture, CreateDocumentAndUseSingleChannelTileAccessorAndCheckResult)
+TEST_P(ZOrderAndResultGray8Fixture, CreateDocumentAndUseSingleChannelTileAccessorWithSortByMAndCheckResult)
 {
     // We create a document with 3 subblocks, where the M-index (of each subblock) is given by the test parameters.
     // The subblocks are 2x1 pixels, and the pixel values are 42 for the 1st, 45 for the second, and 47 for the third
@@ -176,7 +176,7 @@ TEST_P(ZOrderAndResultGray8Fixture, CreateDocumentAndUseSingleChannelTileAccesso
     EXPECT_EQ(p[3], expected[3]);
 }
 
-TEST_P(ZOrderAndResultGray8Fixture, CreateDocumentAndUseSingleChannelPyramidLayerTileAccessorAndCheckResult)
+TEST_P(ZOrderAndResultGray8Fixture, CreateDocumentAndUseSingleChannelPyramidLayerTileAccessorWithSortByMAndCheckResult)
 {
     // We create a document with 3 subblocks, where the M-index (of each subblock) is given by the test parameters.
     // The subblocks are 2x1 pixels, and the pixel values are 42 for the 1st, 45 for the second, and 47 for the third
@@ -226,3 +226,111 @@ INSTANTIATE_TEST_SUITE_P(
     make_tuple(1, 0, 2, array<uint8_t, 4>{ 42, 42, 47, 47 }),
     // second tile on top, first tile in the middle, third tile at the bottom
     make_tuple(1, 2, 0, array<uint8_t, 4>{ 42, 45, 45, 47 })));
+
+TEST(Accessor, CreateDocumentAndUseSingleChannelScalingTileAccessorAndCheckResult)
+{
+    // We create a document with 3 subblocks, where the M-index (of each subblock) is given by the test parameters.
+    // The subblocks are 2x1 pixels, and the pixel values are 42 for the 1st, 45 for the second, and 47 for the third
+    // 1st subblock is at (0,0), 2nd at (0,1) and 3rd at (0,2).
+    // Then we use a single-channel scaling tile accessor to get the tile composite of size 4x1 pixels at (0,0) and check the result.
+    // When doing the tile composite, we instruct NOT to sort by M-index, so the order is undefined and we therefore check that the
+    // result is one of four possible results.
+
+    // arrange
+    auto czi_document_as_blob = CreateTestCziDocumentAndGetAsBlob(array<int, 3>{ 0, 1, 2 });
+    const auto memory_stream = make_shared<CMemInputOutputStream>(get<0>(czi_document_as_blob).get(), get<1>(czi_document_as_blob));
+    const auto reader = CreateCZIReader();
+    reader->Open(memory_stream);
+    const auto accessor = reader->CreateSingleChannelScalingTileAccessor();
+    const CDimCoordinate plane_coordinate{ {DimensionIndex::C, 0} };
+    ISingleChannelScalingTileAccessor::Options options;
+    options.Clear();
+    options.sortByM = false;
+
+    // act
+    const auto composite_bitmap = accessor->Get(PixelType::Gray8, IntRect{ 0,0,4,1 }, &plane_coordinate, 1.f, &options);
+
+    // assert
+    const ScopedBitmapLockerSP lock_info_bitmap{ composite_bitmap };
+    const uint8_t* p = static_cast<const uint8_t*>(lock_info_bitmap.ptrDataRoi);
+    const auto& expected_variant1 = array<uint8_t, 4>{ 42, 45, 47, 47};
+    const auto& expected_variant2 = array<uint8_t, 4>{ 42, 42, 45, 47};
+    const auto& expected_variant3 = array<uint8_t, 4>{ 42, 42, 47, 47};
+    const auto& expected_variant4 = array<uint8_t, 4>{ 42, 45, 45, 47};
+    EXPECT_TRUE(memcmp(p, &expected_variant1, sizeof(expected_variant1)) == 0 ||
+                memcmp(p, &expected_variant2, sizeof(expected_variant2)) == 0 ||
+                memcmp(p, &expected_variant3, sizeof(expected_variant3)) == 0 ||
+                memcmp(p, &expected_variant4, sizeof(expected_variant4)) == 0);
+}
+
+TEST(Accessor, CreateDocumentAndUseSingleChannelTileAccessorAndCheckResult)
+{
+    // We create a document with 3 subblocks, where the M-index (of each subblock) is given by the test parameters.
+    // The subblocks are 2x1 pixels, and the pixel values are 42 for the 1st, 45 for the second, and 47 for the third
+    // 1st subblock is at (0,0), 2nd at (0,1) and 3rd at (0,2).
+    // Then we use a single-channel tile accessor to get the tile composite of size 4x1 pixels at (0,0) and check the result.
+    // When doing the tile composite, we instruct NOT to sort by M-index, so the order is undefined and we therefore check that the
+    // result is one of four possible results.
+
+    // arrange
+    auto czi_document_as_blob = CreateTestCziDocumentAndGetAsBlob(array<int, 3>{ 0, 1, 2 });
+    const auto memory_stream = make_shared<CMemInputOutputStream>(get<0>(czi_document_as_blob).get(), get<1>(czi_document_as_blob));
+    const auto reader = CreateCZIReader();
+    reader->Open(memory_stream);
+    const auto accessor = reader->CreateSingleChannelTileAccessor();
+    const CDimCoordinate plane_coordinate{ {DimensionIndex::C, 0} };
+    ISingleChannelTileAccessor::Options options;
+    options.Clear();
+    options.sortByM = false;
+
+    // act
+    const auto composite_bitmap = accessor->Get(PixelType::Gray8, IntRect{ 0,0,4,1 }, &plane_coordinate, &options);
+
+    // assert
+    const ScopedBitmapLockerSP lock_info_bitmap{ composite_bitmap };
+    const uint8_t* p = static_cast<const uint8_t*>(lock_info_bitmap.ptrDataRoi);
+    const auto& expected_variant1 = array<uint8_t, 4>{ 42, 45, 47, 47};
+    const auto& expected_variant2 = array<uint8_t, 4>{ 42, 42, 45, 47};
+    const auto& expected_variant3 = array<uint8_t, 4>{ 42, 42, 47, 47};
+    const auto& expected_variant4 = array<uint8_t, 4>{ 42, 45, 45, 47};
+    EXPECT_TRUE(memcmp(p, &expected_variant1, sizeof(expected_variant1)) == 0 ||
+                memcmp(p, &expected_variant2, sizeof(expected_variant2)) == 0 ||
+                memcmp(p, &expected_variant3, sizeof(expected_variant3)) == 0 ||
+                memcmp(p, &expected_variant4, sizeof(expected_variant4)) == 0);
+}
+
+TEST(Accessor, CreateDocumentAndUseSingleChannelPyramidLayerTileAccessorWithAndCheckResult)
+{
+    // We create a document with 3 subblocks, where the M-index (of each subblock) is given by the test parameters.
+    // The subblocks are 2x1 pixels, and the pixel values are 42 for the 1st, 45 for the second, and 47 for the third
+    // 1st subblock is at (0,0), 2nd at (0,1) and 3rd at (0,2).
+    // Then we use a pyramid-layer tile accessor to get the tile composite of size 4x1 pixels at (0,0) and check the result.
+    // When doing the tile composite, we instruct NOT to sort by M-index, so the order is undefined and we therefore check that the
+    // result is one of four possible results.
+
+    // arrange
+    auto czi_document_as_blob = CreateTestCziDocumentAndGetAsBlob(array<int, 3>{ 0, 1, 2 });
+    const auto memory_stream = make_shared<CMemInputOutputStream>(get<0>(czi_document_as_blob).get(), get<1>(czi_document_as_blob));
+    const auto reader = CreateCZIReader();
+    reader->Open(memory_stream);
+    const auto accessor = reader->CreateSingleChannelPyramidLayerTileAccessor();
+    CDimCoordinate plane_coordinate{ {DimensionIndex::C, 0} };
+    ISingleChannelPyramidLayerTileAccessor::Options options;
+    options.Clear();
+    options.sortByM = false;
+
+    // act
+    const auto composite_bitmap = accessor->Get(PixelType::Gray8, IntRect{ 0,0,4,1 }, &plane_coordinate, ISingleChannelPyramidLayerTileAccessor::PyramidLayerInfo{ 2,0 }, &options);
+
+    // assert
+    const ScopedBitmapLockerSP lock_info_bitmap{ composite_bitmap };
+    const uint8_t* p = static_cast<const uint8_t*>(lock_info_bitmap.ptrDataRoi);
+    const auto& expected_variant1 = array<uint8_t, 4>{ 42, 45, 47, 47};
+    const auto& expected_variant2 = array<uint8_t, 4>{ 42, 42, 45, 47};
+    const auto& expected_variant3 = array<uint8_t, 4>{ 42, 42, 47, 47};
+    const auto& expected_variant4 = array<uint8_t, 4>{ 42, 45, 45, 47};
+    EXPECT_TRUE(memcmp(p, &expected_variant1, sizeof(expected_variant1)) == 0 ||
+                memcmp(p, &expected_variant2, sizeof(expected_variant2)) == 0 ||
+                memcmp(p, &expected_variant3, sizeof(expected_variant3)) == 0 ||
+                memcmp(p, &expected_variant4, sizeof(expected_variant4)) == 0);
+}
