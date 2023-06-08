bb8433129ebd16aa41b5dd455e836eb225e93c07
ptahmose <jbohl@h-quer.de>
2023-06-08 16:09:41 +0200

    cosmetic

--- Src/libCZI/CziMetadataBuilder.cpp
+++ Src/libCZI/CziMetadataBuilder.cpp
@@ -264,7 +264,7 @@ pugi::xml_node CNodeWrapper::GetChildNodePathMustExist(const char* path)
 
 //--------------------------------------------------------------------------------------
 
-std::shared_ptr<IXmlNodeRw> CNodeWrapper::GetOrCreateChildNodeInternal(const char* path/*, bool allowCreation*/)
+std::shared_ptr<IXmlNodeRw> CNodeWrapper::GetOrCreateChildNodeInternal(const char* path)
 {
     auto p = Utilities::convertUtf8ToWchar_t(path);
 
@@ -280,7 +280,7 @@ std::shared_ptr<IXmlNodeRw> CNodeWrapper::GetOrCreateChildNodeInternal(const cha
         throw LibCZIMetadataBuilderException("invalid path", LibCZIMetadataBuilderException::ErrorType::InvalidPath);
     }
 
-    auto node = this->GetOrCreateChildElementNodeWithAttributes(this->node, tokens[0]/*, allowCreation*/);
+    auto node = this->GetOrCreateChildElementNodeWithAttributes(this->node, tokens[0]);
     if (!node)
     {
         return nullptr;
@@ -288,7 +288,7 @@ std::shared_ptr<IXmlNodeRw> CNodeWrapper::GetOrCreateChildNodeInternal(const cha
 
     for (size_t i = 1; i < tokens.size(); ++i)
     {
-        node = CNodeWrapper::GetOrCreateChildElementNodeWithAttributes(node, tokens[i].c_str()/*, allowCreation*/);
+        node = CNodeWrapper::GetOrCreateChildElementNodeWithAttributes(node, tokens[i]);
         if (!node)
         {
             return nullptr;
@@ -335,12 +335,7 @@ pugi::xml_node CNodeWrapper::GetOrCreatePcDataChild()
     attribute.set_value(Utilities::convertUtf8ToWchar_t(value).c_str());
 }
 
-//pugi::xml_node CNodeWrapper::GetOrCreateChildElementNodeWithAttributes(const std::wstring& str/*, bool allowCreation*/)
-//{
-//    return CNodeWrapper::GetOrCreateChildElementNodeWithAttributes(this->node, str/*, allowCreation*/);
-//}
-
-/*static*/pugi::xml_node CNodeWrapper::GetOrCreateChildElementNodeWithAttributes(pugi::xml_node& node, const std::wstring& str/*, bool allowCreation*/)
+/*static*/pugi::xml_node CNodeWrapper::GetOrCreateChildElementNodeWithAttributes(pugi::xml_node& node, const std::wstring& str)
 {
     std::wregex nodenameWihtAttribregex(LR"(([^\[\]]+)(\[([^\[\]]*)\])?)");
     std::wsmatch pieces_match;
@@ -355,12 +350,12 @@ pugi::xml_node CNodeWrapper::GetOrCreatePcDataChild()
                 if (pieces_match[2].matched == false && pieces_match[3].matched == false)
                 {
                     // we only got a name ( not followed by [Id=abc] )
-                    return GetOrCreateChildElementNode(node, nodeName.c_str()/*, allowCreation*/);
+                    return GetOrCreateChildElementNode(node, nodeName.c_str());
                 }
                 else if (pieces_match[2].matched == true && pieces_match[3].matched == true)
                 {
                     const auto attribValuePairs = CNodeWrapper::ParseAttributes(pieces_match[3]);
-                    return GetOrCreateChildElementNodeWithAttributes(node, nodeName, attribValuePairs/*, allowCreation*/);
+                    return GetOrCreateChildElementNodeWithAttributes(node, nodeName, attribValuePairs);
                 }
             }
         }
@@ -369,9 +364,9 @@ pugi::xml_node CNodeWrapper::GetOrCreatePcDataChild()
     throw LibCZIMetadataBuilderException("invalid path", LibCZIMetadataBuilderException::ErrorType::InvalidPath);
 }
 
-pugi::xml_node CNodeWrapper::GetOrCreateChildElementNode(const wchar_t* sz/*, bool allowCreation*/)
+pugi::xml_node CNodeWrapper::GetOrCreateChildElementNode(const wchar_t* sz)
 {
-    return CNodeWrapper::GetOrCreateChildElementNode(this->node, sz/*, allowCreation*/);
+    return CNodeWrapper::GetOrCreateChildElementNode(this->node, sz);
 }
 
 /*static*/pugi::xml_node CNodeWrapper::GetOrCreateChildElementNodeWithAttributes(pugi::xml_node& node, const std::wstring& str, const std::map<std::wstring, std::wstring>& attribs/*, bool allowCreation*/)
@@ -418,11 +413,6 @@ pugi::xml_node CNodeWrapper::GetOrCreateChildElementNode(const wchar_t* sz/*, bo
     auto c = node.find_child(find_element_node_and_attributes{ str.c_str(), attribs });
     if (!c)
     {
-        //if (!allowCreation)
-        //{
-        //    return xml_node();
-        //}
-
         auto newNode = node.append_child(str.c_str());
         for (auto it = attribs.cbegin(); it != attribs.cend(); ++it)
         {
@@ -435,7 +425,7 @@ pugi::xml_node CNodeWrapper::GetOrCreateChildElementNode(const wchar_t* sz/*, bo
     return c;
 }
 
-/*static*/pugi::xml_node CNodeWrapper::GetOrCreateChildElementNode(pugi::xml_node& node, const wchar_t* sz/*, bool allowCreation*/)
+/*static*/pugi::xml_node CNodeWrapper::GetOrCreateChildElementNode(pugi::xml_node& node, const wchar_t* sz)
 {
     struct find_element_node
     {
@@ -455,11 +445,6 @@ pugi::xml_node CNodeWrapper::GetOrCreateChildElementNode(const wchar_t* sz/*, bo
     auto c = node.find_child(find_element_node{ sz });
     if (!c)
     {
-        /*if (!allowCreation)
-        {
-            return xml_node();
-        }*/
-
         return node.append_child(sz);
     }
 
@@ -589,7 +574,7 @@ CCZiMetadataBuilder::CCZiMetadataBuilder(const wchar_t* rootNodeName, const std:
 
         s.append("]");
 
-        auto channelNode = root->GetOrCreateChildNode(s.c_str());
+        const auto channelNode = root->GetOrCreateChildNode(s.c_str());
 
         auto pxlTypeIterator = pixelTypeForChannel.GetChannelIndexPixelTypeMap().find(ch);
         if (pxlTypeIterator != pixelTypeForChannel.GetChannelIndexPixelTypeMap().end())
@@ -658,7 +643,7 @@ CCZiMetadataBuilder::CCZiMetadataBuilder(const wchar_t* rootNodeName, const std:
         break;
     default:
         return false;
-    };
+    }
 
     return true;
 }
@@ -714,7 +699,6 @@ bool libCZI::XmlDateTime::IsValid() const
 /*static*/bool libCZI::XmlDateTime::TryParse(const char* sz, libCZI::XmlDateTime* ptrDateTime)
 {
     // cf. https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s07.html
-    //static const char* regexStr = "^(?<year>-?(?:[1-9][0-9]*)?[0-9]{4})-(?<month>1[0-2]|0[1-9])-(?<day>3[01]|0[1-9]|[12][0-9])T(?<hour>2[0-3]|[01][0-9]):(?<minute>[0-5][0-9]):(?<second>[0-5][0-9])(?<ms>\.[0-9]+)?(?<timezone>Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$";
     static const char* regexStr = R"((-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?)";
 
     auto str = Utilities::Trim(sz);
