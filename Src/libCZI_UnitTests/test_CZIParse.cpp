589ad4868b125fe0b61b46002829647528d09036
ptahmose <jbohl@h-quer.de>
2023-09-04 00:58:10 +0200

    fix gcc build error

--- Src/libCZI_UnitTests/test_CZIParse.cpp
+++ Src/libCZI_UnitTests/test_CZIParse.cpp
@@ -54,7 +54,7 @@ TEST(CZIParse, ParseSubblockDirectoryWithSubblockWithSizeMOf2AndExpectException)
 
     // ...however, if we choose to ignore the sizeM-entry, we expect no exception
     parse_options.SetDimensionMMustHaveSizeOne(false);
-    EXPECT_NO_THROW(CCZIParse::ReadSubBlockDirectory(memory_stream.get(), file_header_segment_data.GetSubBlockDirectoryPosition(), parse_options), LibCZICZIParseException);
+    EXPECT_NO_THROW(CCZIParse::ReadSubBlockDirectory(memory_stream.get(), file_header_segment_data.GetSubBlockDirectoryPosition(), parse_options));
 
     // ...and, if we choose to ignore only pyramid-subblocks, we still expect an exception (since the subblock is not a pyramid-subblock)
     parse_options.SetDimensionMMustHaveSizeOneExceptForPyramidSubblocks(true);
