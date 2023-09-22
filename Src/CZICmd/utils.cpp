40e6404b6f8987233e197fa879ae772caff03503
ptahmose <jbohl@h-quer.de>
2023-09-22 23:14:27 +0200

    fix

--- Src/CZICmd/utils.cpp
+++ Src/CZICmd/utils.cpp
@@ -22,23 +22,6 @@
 
 using namespace std;
 
-std::ostream& operator<<(std::ostream& os, const GUID& guid)
-{
-    os << std::hex << std::setfill('0')
-        << std::setw(8) << guid.Data1 << '-'
-        << std::setw(4) << guid.Data2 << '-'
-        << std::setw(4) << guid.Data3 << '-'
-        << std::setw(2) << static_cast<int>(guid.Data4[0])
-        << std::setw(2) << static_cast<int>(guid.Data4[1]) << '-'
-        << std::setw(2) << static_cast<int>(guid.Data4[2])
-        << std::setw(2) << static_cast<int>(guid.Data4[3])
-        << std::setw(2) << static_cast<int>(guid.Data4[4])
-        << std::setw(2) << static_cast<int>(guid.Data4[5])
-        << std::setw(2) << static_cast<int>(guid.Data4[6])
-        << std::setw(2) << static_cast<int>(guid.Data4[7]);
-    return os;
-}
-
 std::string convertToUtf8(const std::wstring& str)
 {
 #if defined(HAS_CODECVT)
