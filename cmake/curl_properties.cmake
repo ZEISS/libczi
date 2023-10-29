28c771f030c4defb708f8b840bf03a2a93a99345
ptahmose <jbohl@h-quer.de>
2023-10-29 20:57:35 +0100

    test

--- cmake/curl_properties.cmake
+++ cmake/curl_properties.cmake
@@ -1,6 +1,6 @@
  
  # Set properties for the imported target
    set_target_properties(CURL::libcurl PROPERTIES
-    IMPORTED_LOCATION ${binary_dir}
+    IMPORTED_LOCATION "${CMAKE_BINARY_DIR}/curl/lib"
     INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/curl/include"
     )
