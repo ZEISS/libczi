8cb7ac30fb9982f83d91b76f3730a6f8862a46b0
ptahmose <jbohl@h-quer.de>
2023-11-03 16:58:51 +0100

    enable private RapidJSON-download & improvements to CMake (#72)
    * clang-tidy fixes
    
    * cosmetic
    
    * add option "LIBCZI_BUILD_PREFER_EXTERNALPACKAGE_RAPIDJSON", fix libCURL configuration
    
    * update
    
    * this seems to work ok - Mingw build is now fine
    
    * update
    
    * cosmetic
    
    * cosmetic
    
    * bump version
    
    * Update Src/CZICmd/CMakeLists.txt
    
    Co-authored-by: DaveyJonesBitPail <119518234+DaveyJonesBitPail@users.noreply.github.com>
    
    * Update Src/CZICmd/CMakeLists.txt
    
    Co-authored-by: DaveyJonesBitPail <119518234+DaveyJonesBitPail@users.noreply.github.com>
    
    * language
    
    ---------
    
    Co-authored-by: DaveyJonesBitPail <119518234+DaveyJonesBitPail@users.noreply.github.com>

--- Src/CZICmd/platform_defines.h
+++ Src/CZICmd/platform_defines.h
@@ -5,10 +5,9 @@
 #pragma once
 
 #if defined(_WIN32)
-// this means that the Win32-API is available, and the Windows-SDK is available
-#define WIN32ENV (1)
-#endif
-
-#if defined(__GNUC__)
-#define LINUXENV (1)
+    // this means that the Win32-API is available, and the Windows-SDK is available
+    #define WIN32ENV (1)
+#else
+    // otherwise, we assume that we are on Linux
+    #define LINUXENV (1)
 #endif
