89334e1ee36bdfdb0cff2b73d8d276f6dcf50573
ptahmose <jbohl@h-quer.de>
2023-11-11 23:37:02 +0100

    cleanup

--- Src/libCZI/CreateBitmap.cpp
+++ Src/libCZI/CreateBitmap.cpp
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-#include <chrono>
-
 #include "bitmapData.h"
 #include "Site.h"
 #include "libCZI.h"
@@ -48,7 +46,7 @@ static std::shared_ptr<libCZI::IBitmapData> CreateBitmapFromSubBlock_Uncompresse
 
     const auto& sbBlkInfo = subBlk->GetSubBlockInfo();
 
-    // TODO: How exactly shoud the stride be derived? It seems that stride must be exactly linesize.
+    // The stride with an uncompressed bitmap in CZI is exactly the linesize.
     const std::uint32_t stride = sbBlkInfo.physicalSize.w * CziUtils::GetBytesPerPel(sbBlkInfo.pixelType);
     if (static_cast<size_t>(stride) * sbBlkInfo.physicalSize.h > size)
     {
@@ -74,36 +72,17 @@ static std::shared_ptr<libCZI::IBitmapData> CreateBitmapFromSubBlock_Uncompresse
 
 std::shared_ptr<libCZI::IBitmapData> libCZI::CreateBitmapFromSubBlock(ISubBlock* subBlk)
 {
-    //auto start = std::chrono::high_resolution_clock::now();
-    std::shared_ptr<libCZI::IBitmapData> bitmapData;
     switch (subBlk->GetSubBlockInfo().GetCompressionMode())
     {
     case CompressionMode::JpgXr:
-        bitmapData = CreateBitmapFromSubBlock_JpgXr(subBlk);
-        break;
+        return CreateBitmapFromSubBlock_JpgXr(subBlk);
     case CompressionMode::Zstd0:
-        bitmapData = CreateBitmapFromSubBlock_ZStd0(subBlk);
-        break;
+        return CreateBitmapFromSubBlock_ZStd0(subBlk);
     case CompressionMode::Zstd1:
-        bitmapData = CreateBitmapFromSubBlock_ZStd1(subBlk);
-        break;
+        return CreateBitmapFromSubBlock_ZStd1(subBlk);
     case CompressionMode::UnCompressed:
-        bitmapData = CreateBitmapFromSubBlock_Uncompressed(subBlk);
-        break;
+        return CreateBitmapFromSubBlock_Uncompressed(subBlk);
     default:    // silence warnings
         throw std::logic_error("The method or operation is not implemented.");
     }
-   /* auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    if (GetSite()->IsEnabled(LOGLEVEL_CHATTYINFORMATION))
-    {
-        IntSize extent = bitmapData->GetSize();
-        int64_t data_size = static_cast<int64_t>(extent.w) * extent.h * CziUtils::GetBytesPerPel(bitmapData->GetPixelType());
-        std::stringstream ss;
-        double elapsed_time = elapsed_seconds.count();
-        ss << "CreateBitmap: " << elapsed_time * 1000.0 << "ms, size: " << data_size / 1e6 << "MB -> " << (data_size / elapsed_time) / 1e6 << "MB/s";
-        GetSite()->Log(LOGLEVEL_CHATTYINFORMATION, ss);
-    }*/
-
-    return bitmapData;
 }
