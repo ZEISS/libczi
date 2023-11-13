bf3c35ba9e965f1a61edc7b7af28a3e6bc054b7a
ptahmose <jbohl@h-quer.de>
2023-11-13 23:20:18 +0100

    Update Src/libCZI/SingleChannelAccessorBase.h
    Co-authored-by: DaveyJonesBitPail <119518234+DaveyJonesBitPail@users.noreply.github.com>

--- Src/libCZI/SingleChannelAccessorBase.h
+++ Src/libCZI/SingleChannelAccessorBase.h
@@ -28,7 +28,7 @@ protected:
     ///   If called with value 0, the subblocks is the **last** one to be rendered, if called with value 1, the subblock is the
     ///   second-last one to be rendered, and so on.
     ///   The value it returns is the subblock index (in the subblock repository) to check.
-    /// - The subblocks are assumed to be rendered in the order given, so the one we get by calling  'get_subblock_index' with  
+    /// - The subblocks are assumed to be rendered in the order given, so the one we get by calling 'get_subblock_index' with  
     ///   argument 0 is the first one to be rendered, the one with argument 1 is the second one, and so on. The rendering 
     ///   is assumed to be done with the 'painter's algorithm", so what is rendered last is on top.
     /// - We return a list of indices which are to be rendered, potentially leaving out some which have been determined  
