31026ee146a2dc14e0af9a58f3be2780a2b3a5b8
ptahmose <jbohl@h-quer.de>
2023-11-15 21:47:43 +0100

    add option "kCurlHttp_FollowLocation"

--- Src/libCZI/StreamsLib/curlhttpinputstream.cpp
+++ Src/libCZI/StreamsLib/curlhttpinputstream.cpp
@@ -159,11 +159,18 @@ CurlHttpInputStream::CurlHttpInputStream(const std::string& url, const std::map<
         ThrowIfCurlSetOptError(return_code, "CURLOPT_SSL_VERIFYPEER");
     }
 
-    property = property_bag.find(StreamsFactory::StreamProperties::kCurlHttp_SslVerifyHost);
+    property = property_bag.find(StreamsFactory::StreamProperties::kCurlHttp_FollowLocation);
     if (property != property_bag.end())
     {
-        return_code = curl_easy_setopt(up_curl_handle.get(), CURLOPT_SSL_VERIFYHOST, property->second.GetAsBoolOrThrow() ? 1L : 0L);
-        ThrowIfCurlSetOptError(return_code, "CURLOPT_SSL_VERIFYHOST");
+        return_code = curl_easy_setopt(up_curl_handle.get(), CURLOPT_FOLLOWLOCATION, property->second.GetAsBoolOrThrow() ? 1L : 0L);
+        ThrowIfCurlSetOptError(return_code, "CURLOPT_FOLLOWLOCATION");
+    }
+
+    property = property_bag.find(StreamsFactory::StreamProperties::kCurlHttp_MaxRedirs);
+    if (property != property_bag.end())
+    {
+        return_code = curl_easy_setopt(up_curl_handle.get(), CURLOPT_MAXREDIRS, property->second.GetAsInt32OrThrow());
+        ThrowIfCurlSetOptError(return_code, "CURLOPT_MAXREDIRS");
     }
 
     this->curl_handle_ = up_curl_handle.release();
