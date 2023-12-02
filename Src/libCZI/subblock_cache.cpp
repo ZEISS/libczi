e9880ac5e66b6edc7cfd9c691599f129ff1bf0bc
ptahmose <jbohl@h-quer.de>
2023-12-02 12:14:00 +0100

    update

--- Src/libCZI/subblock_cache.cpp
+++ Src/libCZI/subblock_cache.cpp
@@ -18,20 +18,40 @@ SubBlockCache::~SubBlockCache()
 std::shared_ptr<IBitmapData> SubBlockCache::Get(int subblock_index)
 {
     lock_guard<mutex> lck(this->mutex_);
-    auto element = this->cache_.find(subblock_index);
+    const auto element = this->cache_.find(subblock_index);
     if (element != this->cache_.end())
     {
         element->second.last_used = this->usage_counter_.fetch_add(1);
         return element->second.bitmap;
     }
-    else
-    {
-        return {};
-    }
+
+    return {};
 }
 
 void SubBlockCache::Add(int subblock_index, std::shared_ptr<IBitmapData> bitmap)
 {
+    const auto size_in_bytes_of_added_bitmap = SubBlockCache::CalculateSizeInBytes(bitmap.get());
+    const auto entry_to_be_added = CacheEntry{ bitmap, this->usage_counter_.fetch_add(1) };
+
     lock_guard<mutex> lck(this->mutex_);
-    this->cache_[subblock_index] = { bitmap, this->usage_counter_.fetch_add(1) };
+    const auto result = this->cache_.insert({ subblock_index, entry_to_be_added });
+    if (result.second)
+    {
+        // New element inserted
+        this->cache_size_in_bytes_ += size_in_bytes_of_added_bitmap;
+        ++this->cache_subblock_count_;
+    }
+    else
+    {
+        // Element with the same key already existed
+        this->cache_size_in_bytes_ -= SubBlockCache::CalculateSizeInBytes(result.first->second.bitmap.get());
+        result.first->second = entry_to_be_added;
+        this->cache_size_in_bytes_ += size_in_bytes_of_added_bitmap;
+    }
+}
+
+/*static*/std::uint64_t SubBlockCache::CalculateSizeInBytes(const libCZI::IBitmapData* bitmap)
+{
+    const IntSize size = bitmap->GetSize();
+    return static_cast<uint64_t>(size.w) * size.h * Utils::GetBytesPerPixel(bitmap->GetPixelType());
 }
--- Src/libCZI/subblock_cache.h
+++ Src/libCZI/subblock_cache.h
@@ -22,10 +22,15 @@ private:
     std::map<int, CacheEntry> cache_;
     std::mutex mutex_;
     std::atomic_uint64_t usage_counter_{ 0 };
+    std::atomic_uint64_t cache_size_in_bytes_{ 0 };
+    std::atomic_uint32_t cache_subblock_count_{ 0 };
 public:
     SubBlockCache(const libCZI::ISubBlockCache::Options& options);
     ~SubBlockCache();
 
     std::shared_ptr<libCZI::IBitmapData> Get(int subblock_index) override;
     void Add(int subblock_index, std::shared_ptr<libCZI::IBitmapData> bitmap) override;
+
+private:
+    static std::uint64_t CalculateSizeInBytes(const libCZI::IBitmapData* bitmap);
 };
