09a952b994b4cdcecd9c2f2532fbf5106a578cb8
ptahmose <jbohl@h-quer.de>
2024-09-13 17:57:12 +0200

    cosmetic, remove option "LIBCZI_BUILD_PREFER_EXTERNALPACKAGE_AZURESDK"

--- cmake/dummy/umock_cConfig.cmake
+++
@@ -1,3 +0,0 @@
-# Dummy wilConfig.cmake to satisfy find_package(wil)
-#include_directories(${CMAKE_CURRENT_LIST_DIR}/path/to/wil/include)
-include_directories("D:/dev/Github/libczi-zeiss-ptahmose/build/_deps/umock-c-src")
