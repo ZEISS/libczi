ba4ab85bf6fdafb50359c53c4aaeed44c03cf9b4
ptahmose <jbohl@h-quer.de>
2025-04-15 13:46:44 +0200

    change default of "LIBCZI_BUILD_LIBCZIAPI" (to off)

--- Src/libCZIAPI_UnitTests/main.cpp
+++ Src/libCZIAPI_UnitTests/main.cpp
@@ -8,11 +8,6 @@ int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
 
-    /*
-    // perform the "one-time initialization" of the libCZI streams factory
-    libCZI::StreamsFactory::Initialize();
-    */
-
     const int ret = RUN_ALL_TESTS();
     return ret;
 }
