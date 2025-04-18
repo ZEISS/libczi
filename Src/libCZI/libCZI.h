34b457ab533c28e3cea4406ff6bf7171f4d7e841
ptahmose <jbohl@h-quer.de>
2025-04-18 16:28:58 +0200

    cosmetic

--- Src/libCZI/libCZI.h
+++ Src/libCZI/libCZI.h
@@ -749,7 +749,7 @@ namespace libCZI
 
             /// This option controls whether the lax parameter validation when parsing the dimension-entry of a subblock is to be used.
             /// Previous versions of libCZI did not check whether certain values in the file have the expected value. If those values
-            /// are different than expected, this meant that libCZI would not be able to deal with the document properly.  
+            /// are different than expected, this meant that libCZI would not be able to deal with the document properly.
             /// If lax checking of this is disabled, then Open will fail with a corresponding exception.
             /// The default is to enable lax checking (for compatibility with previous libCZI-versions), but users are encouraged to
             /// disable this for new code.
@@ -765,12 +765,12 @@ namespace libCZI
 
             /// The default frame-of-reference which is to be used by the reader-object. This determines which frame-of-reference
             /// is used when the enum value "CZIFrameOfReference::Default" is used with an operation of the reader-object.
-            /// If the value specified here is "CZIFrameOfReference::Invalid" or "CZIFrameOfReference::Default", then 
+            /// If the value specified here is "CZIFrameOfReference::Invalid" or "CZIFrameOfReference::Default", then
             /// "CZIFrameOfReference::RawSubBlockCoordinateSystem" will be used.
             libCZI::CZIFrameOfReference default_frame_of_reference{ libCZI::CZIFrameOfReference::Invalid };
 
             /// This bitfield is used to specify the policy which information is considered authoritative in the construction of a sub-block -
-            /// either the information in the sub-block directory or in the sub-block header. Also, it controls how to handle a discrepancy 
+            /// either the information in the sub-block directory or in the sub-block header. Also, it controls how to handle a discrepancy
             /// in this respect - either throw an exception if a discrepancy is encountered or ignore it.
             SubBlockDirectoryInfoPolicy subBlockDirectoryInfoPolicy{ SubBlockDirectoryInfoPolicy::SubBlockDirectoryPrecedence };
 
@@ -802,7 +802,7 @@ namespace libCZI
 
         /// Reads the metadata segment from the stream.
         /// \remark
-        /// If the class is not operational (i. e. Open was not called or Open was not successful), then an exception of type std::logic_error is thrown.
+        /// If the class is not operational (i.e. Open was not called or Open was not successful), then an exception of type std::logic_error is thrown.
         ///
         /// \return The metadata segment.
         virtual std::shared_ptr<IMetadataSegment> ReadMetadataSegment() = 0;
@@ -820,7 +820,7 @@ namespace libCZI
         /// Closes CZI-reader. The underlying stream-object will be released, and further calls to
         /// other methods will fail. The stream is also closed when the object is destroyed, so it
         /// is usually not necessary to explicitly call `Close`. Note that the stream is not closed
-        /// immediately (or - there is no guarantee that on return from this call all references to the 
+        /// immediately (or - there is no guarantee that on return from this call all references to the
         /// stream object are released). Concurrently executing operations continue to use the stream
         /// and keep it referenced until they are finished.
         virtual void Close() = 0;
