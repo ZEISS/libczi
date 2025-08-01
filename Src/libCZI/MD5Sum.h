0f595c0b7176dbfa67826ed8f65162fa68c5fa76
ptahmose <jbohl@h-quer.de>
2025-08-01 12:24:42 +0200

    Refactor MD5 structures for type safety
    Updated member variable types in `md5_context` and `md5_digest` from `unsigned int` and `unsigned char` to `std::uint32_t` and `std::uint8_t`. This change improves type safety and ensures consistent use of fixed-width integer types, enhancing portability and clarity in the code.

--- Src/libCZI/MD5Sum.h
+++ Src/libCZI/MD5Sum.h
@@ -22,21 +22,21 @@ private:
     struct md5_context
     {
         // state
-        unsigned int a;
-        unsigned int b;
-        unsigned int c;
-        unsigned int d;
+        std::uint32_t a;
+        std::uint32_t b;
+        std::uint32_t c;
+        std::uint32_t d;
         // number of bits, modulo 2^64 (lsb first)
-        unsigned int count[2];
+        std::uint32_t count[2];
         // input buffer
-        unsigned char input[64];
+        std::uint8_t input[64];
         // current block
-        unsigned int block[16];
+        std::uint32_t block[16];
     };
 
     struct md5_digest
     {
-        unsigned char bytes[MD5_HASH_SIZE];
+        std::uint8_t bytes[MD5_HASH_SIZE];
     };
 
     md5_context ctx_;
